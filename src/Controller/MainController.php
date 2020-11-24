95684165baf9e1057fdfda8553aeaed84388c421
orochefeuille <rochefeuille.o@gmail.com>
2020-11-25 00:01:32 +0100

    Feature: Prohibit webapp acces to anonymous

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -6,11 +6,16 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 
+/**
+*
+* @IsGranted("ROLE_USER")
+*/
 class MainController extends AbstractController
 {
     /**
-     * @Route("/", name="home")
+     * @Route("/", name="app_home")
      */
     public function index(): Response
     {
